00bb9ebaf63d50d08acf17531e4f4c967ab9e723
Muhammed Fatih Çınar <fatih.cinar@trendyol.com>
2022-04-18 00:05:19 +0300

    create post details component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,2 +1,3 @@
 export { default as RecommendedPosts } from './RecommendedPosts/RecommendedPosts';
 export { default as RecommendedPostsBanner } from './RecommendedPostsBanner/RecommendedPostsBanner';
+export { default as PostDetails } from './PostDetails/PostDetails';
