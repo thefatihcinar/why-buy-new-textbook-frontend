d6e33b5467bf62b8afe1721ce31eefcfc86016f3
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-22 00:20:17 +0300

    implement the request interceptor

---
+++ src/agents/interceptors/requestInterceptor.js
@@ -0,0 +1,26 @@
+import { AuthenticationService } from "../../services";
+
+/* This class is used to intercept the request and modify it before sending it to the server */
+/* What it does basically is that, it adds the token to the request header */
+class TheRequestInterceptor {
+
+    onRejected(error) {
+        return Promise.reject(error);
+    }
+
+    onFulfilled(config){
+        // get the auth token from the local storage
+        // if not, send this requests without token, because the main page allows it
+        const authToken = AuthenticationService.getAuthToken();
+
+        if(authToken){
+            /* if the token is existing, in other words, if the user has logged in */
+            config.headers.authorization = `Bearer ${authToken}`;
+            /* put the token in the header, before each request */
+        }
+
+        return config;
+    }
+}
+
+export default TheRequestInterceptor;
