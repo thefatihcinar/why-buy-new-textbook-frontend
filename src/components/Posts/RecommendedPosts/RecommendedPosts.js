31cd1b8a01cbce0be405585b665483fcaa40b57f
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-23 23:26:39 +0300

    getting recommended posts from posts service in recommended posts component

--- src/components/Posts/RecommendedPosts/RecommendedPosts.js
+++ src/components/Posts/RecommendedPosts/RecommendedPosts.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState, useEffect} from "react";
+/* Services */
+import PostsService from "../../../services/posts.service";
 
 const RecommendedPosts = () => {
 
+    /* Use local state to store the posts */
+    const [recommendedPosts, setRecommendedPosts] = useState([]);
+
+    /* When the component is mounted, get the recommended posts */
+    useEffect(async () => {
+        const posts = await PostsService.getRecommendedPosts();
+        setRecommendedPosts(posts);
+    }, [recommendedPosts]);
+
+
     return (
-        <div>RecommendedPosts</div>
+        <div>
+            {
+                recommendedPosts && recommendedPosts.map((post) => (
+                <>
+                    <p>{post.title}</p>
+                    <br/>
+                </>
+
+            ))}
+        </div>
     )
 }
 
