af6aeed2279ed829ab0e2e82a47da44b5c6fd0ca
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-23 23:46:54 +0300

    creation of InternalServerError component

---
+++ src/pages/InternalServerError/InternalServerError.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const InternalServerError = () => {
+
+    return (
+        <div>500 - InternalServerError</div>
+    )
+}
+
+export default InternalServerError;
---
+++ src/pages/InternalServerError/index.js
@@ -0,0 +1,1 @@
+export { default as InternalServerError } from './InternalServerError';
