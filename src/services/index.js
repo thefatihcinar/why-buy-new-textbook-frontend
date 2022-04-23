37589ed61d975512a16dd23256113794a4e960ef
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-23 23:53:54 +0300

    export all services in services folder

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,4 @@
 export { default as AuthenticationService } from './authentication.service';
+export { default as PostsService } from './post.service';
+export { default as UserService } from './user.service';
+export { default as FormService } from './form.service';
