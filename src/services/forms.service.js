3c2bdfab594fc98cf6a66168932a38c39d4deb54
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-24 00:03:08 +0300

    reimplementation of forms service with the new http agent

--- src/services/forms.service.js
+++ src/services/forms.service.js
@@ -1,29 +1,41 @@
-/* 
+import { FormsBroker } from "../agents";
+
+/*
   This class is responsible for fetching all the data
   about forms that are going to be used in this application.
 */
 class FormsService {
 
-  static async getCreatePostFormData() {
-    // Provide token.
-    // GET {{ API_URL }}/forms/create-post
+  async getCreatePostFormData() {
+    /* this service is responsible for getting the form data when creating
+    a new post */
+
+    const createPostFormData = await FormsBroker.get("/create-post", {});
+    return createPostFormData;
   }
 
-  static async getRegisterFormData(){
-    // No token needed.
-    // GET {{ API_URL }}/forms/register
+  async getRegisterFormData(){
+    /* this service is responsible for getting the form data
+      when registering a new user */
+
+    const registerFormData = await FormsBroker.get("/register", {});
+    return registerFormData;
   }
 
-  static async getUpdatePostFormData(){
-    // Provide token.
-    // GET {{ API_URL }}/forms/update-post
+  async getUpdatePostFormData(){
+    /* this service is responsible for getting the form data when updating
+    a post */
+    const updatePostFormData = await FormsBroker.get("/update-post", {});
+    return updatePostFormData;
   }
-  
-  static async getSearchPageData(){
-    // No token needed.
-    // GET {{ API_URL }}/forms/search
+
+  async getSearchPageData(){
+    /* this service is responsible for getting the form data
+      when searching for posts */
+    const searchPageData = await FormsBroker.get("/search", {});
+    return searchPageData;
   }
-  
+
 }
 
-export default FormsService;
+export default new FormsService();
