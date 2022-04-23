f29e63a2ddb8a7a044d61b1715ddfbc213d84099
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-24 00:09:02 +0300

    implementation of users service

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,28 +1,42 @@
+import { UserBroker } from "../agents";
 
 /* This class is responsible for all the user related operations
   such as login, register, get user profile, update user profile, etc.
 */
 class UsersService {
-  static async getUserProfile() {
-    // Provide token.
-    // GET {{ API_URL }}/users/profile
+
+  async getUserProfile() {
+    /* this service is responsible for getting the user profile data from the server */
+    const user = await UserBroker.get("/profile", {});
+    // TO-DO: What if the user is not logged in?
+    return user;
   }
 
-  static async updateUserProfile(updatedUser) {
-    // Provide token.
-    // PUT {{ API_URL }}/users/profile
+  async updateUserProfile(updatedUser) {
+    /* this service is responsible for updating the user profile data on the server */
+    const justUpdatedUser = await UserBroker.put("/profile", updatedUser, {});
+    // TO-DO: What if the user is not logged in?
+    return justUpdatedUser;
   }
 
-  static async loginUser(email, password) {
+  async loginUser(email, password) {
     // No token needed.
     // POST {{ API_URL }}/users/login
+    /* this service is responsible for logging in the user */
+    const loginParameters = {
+      email,
+      password
+    };
+    const loginResponse = await UserBroker.post("/login", loginParameters, {});
+
+    return loginResponse;
   }
 
-  static async registerUser(newUser) {
-    // No token needed.
-    // POST {{ API_URL }}/users/
+  async registerUser(newUser) {
+    /* this service is responsible for registering a new user */
+    const createdUser = await UserBroker.post("/", newUser, {});
+    return createdUser;
   }
-  
 }
 
-export default UsersService;
+export default new UsersService();
