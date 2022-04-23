745c28accaca7f71c3052066320105b6190d50ea
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-23 23:55:24 +0300

    initial implementation of PostDetails component

--- src/components/Posts/PostDetails/PostDetails.js
+++ src/components/Posts/PostDetails/PostDetails.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+/* Services */
+import { PostsService } from "../../../services";
 
-const PostDetails = () => {
+const PostDetails = ( { history, match } ) => {
+
+    /* Use local state to store the post */
+    const [post, setPost] = useState({});
+
+    /* When the component is mounted, get the post */
+    useEffect( async () => {
+        /* get the post id from the route */
+        const { id: postID } = match.params;
+        const thePost = await PostsService.getSpecificPost(postID);
+        setPost(thePost);
+
+    }, [post]);
 
     return (
-        <div>PostDetails</div>
+        <div>
+            {
+                post &&
+                <div>
+                    <h3>--- DETAYLARRR -- -</h3>
+                    <h3>{post.title}</h3>
+                    <p>{post.description}</p>
+                </div>
+            }
+        </div>
     )
 }
 
