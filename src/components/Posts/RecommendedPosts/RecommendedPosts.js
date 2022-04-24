cc9b45b8c954891674ec95caef280f480d34feb5
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-24 06:00:07 +0300

    import PostInRow without destructuring in recommended posts

--- src/components/Posts/RecommendedPosts/RecommendedPosts.js
+++ src/components/Posts/RecommendedPosts/RecommendedPosts.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState, useEffect} from "react";
+/* Material UI */
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 /* Services */
 import PostsService from "../../../services/posts.service";
 /* Components */
-import { PostInRow } from "../PostInRow";
+import PostInRow from "../PostInRow";
 
 const RecommendedPosts = () => {
 
@@ -18,13 +21,34 @@ const RecommendedPosts = () => {
 
 
     return (
-        <div>
-            {
-                recommendedPosts && recommendedPosts.map((post) => (
-                <PostInRow key={post.id} post={post} />
-            ))}
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    {
+                        recommendedPosts && recommendedPosts.map((post) => (
+                            <PostInRow key={post.id} post={post} />
+                        ))}
+                </Grid>
+            </Grid>
+        </Box>
+
     )
 }
+/*
+export default function BasicGrid() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    {
+                        recommendedPosts && recommendedPosts.map((post) => (
+                            <PostInRow key={post.id} post={post} />
+                        ))}
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+*/
 
 export default RecommendedPosts;
