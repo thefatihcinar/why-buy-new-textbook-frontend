74c204f6fd8afeedd659976d132ca49b396f7656
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-24 04:13:20 +0300

    creation of OurAims component

---
+++ src/components/OurAims/OurAims.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const OurAims = () => {
+
+    return (
+        <div>OurAims</div>
+    )
+}
+
+export default OurAims;
---
+++ src/components/OurAims/index.js
@@ -0,0 +1,1 @@
+export { default as OurAims } from './OurAims';
