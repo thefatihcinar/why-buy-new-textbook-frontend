deb43a1f32d4b896f806d830464a797762c71b6d
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-24 04:13:57 +0300

    when the default route is hit, render homepage instead of recommended posts

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 /* Components */
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import {PostDetails, RecommendedPosts } from "./components/Posts";
+import { PostDetails  } from "./components/Posts";
+import { PostInRow } from "./components/Posts/PostInRow";
+/* Pages */
+import { HomePage } from './pages';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header/>
+        <PostInRow />
         <Routes>
-          <Route path="/" exact element={ <RecommendedPosts/> }/>
+          <Route path="/" exact element={ <HomePage/> }/>
           <Route path="/posts/:id" element={ <PostDetails/> }/>
         </Routes>
         <Footer/>
