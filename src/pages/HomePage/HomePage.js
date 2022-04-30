78f649087ec59da57f8cbca366b4bfb1c42d2e4e
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-30 17:29:21 +0300

    strip material ui in HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,4 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-
+import React from 'react';
 /* Components */
 import { RecommendedPosts } from "../../components/Posts";
 import { OurAims } from "../../components/OurAims";
@@ -9,16 +6,10 @@ import { OurAims } from "../../components/OurAims";
 
 const HomePage = () => {
     return (
-        <Box sx={{ flexGrow: 1, margin: "1.25%" }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={9} style={{ padding: '0.85rem'}}>
-                    <RecommendedPosts/>
-                </Grid>
-                <Grid item xs={0} md={3}>
-                    <OurAims/>
-                </Grid>
-            </Grid>
-        </Box>
+        <div>
+            <RecommendedPosts/>
+            <OurAims/>
+        </div>
     );
 }
 
