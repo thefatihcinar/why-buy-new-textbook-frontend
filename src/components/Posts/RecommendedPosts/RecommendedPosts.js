c230bff216d657deda5d116e324f0396358c5ce3
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-30 23:41:24 +0300

    re-implementation of layout in RecommendedPosts component, based on Bootstrap

--- src/components/Posts/RecommendedPosts/RecommendedPosts.js
+++ src/components/Posts/RecommendedPosts/RecommendedPosts.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import { useState, useEffect} from "react";
+/* React Bookstrap */
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 /* Services */
 import PostsService from "../../../services/posts.service";
-/* Components */
-import PostInRow from "../PostInRow";
+
+
+const PostInRow = ( {post} ) => {
+    return (
+        <Card className='my-3 p-3 rounded'>
+            <a href = {`/posts/${post._id}`}>
+                <Card.Img src={post.mainImage} variant='top'/>
+            </a>
+            <Card.Body as = 'div'>
+                <a href = {`/posts/${post._id}`}>
+                    <Card.Title>
+                        <strong>{post.title}</strong>
+                    </Card.Title>
+                </a>
+                <Card.Text as = 'div'>
+                    <div className = 'mt-3'>
+                        {post.description}
+                    </div>
+                </Card.Text>
+                <Card.Text>
+                    {post.price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
 
 const RecommendedPosts = () => {
 
@@ -16,35 +45,21 @@ const RecommendedPosts = () => {
         setRecommendedPosts(posts);
     }, [recommendedPosts]);
 
-
     return (
         <div>
-            <div>
-                <div>
-                    {
-                        recommendedPosts && recommendedPosts.map((post) => (
-                            <PostInRow key={post.id} post={post} />
-                        ))}
-                </div>
-            </div>
+            <Container>
+                <h1>Recommended Products</h1>
+                <Row>
+                    {   recommendedPosts &&
+                        recommendedPosts.map( post => (
+                        <Col key={post._id}>
+                            <PostInRow key = {post._id} post = {post}/>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     )
 }
-/*
-export default function BasicGrid() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    {
-                        recommendedPosts && recommendedPosts.map((post) => (
-                            <PostInRow key={post.id} post={post} />
-                        ))}
-                </Grid>
-            </Grid>
-        </Box>
-    );
-}
-*/
 
 export default RecommendedPosts;
