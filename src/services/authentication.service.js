298158c94dc6a86997235900c0b1a4df215ea643
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-04-30 22:32:42 +0300

    decode token when getting isAuthenticated information

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,3 +1,5 @@
+import decode from 'jwt-decode';
+
 /*
     This Authentication Service Class is responsible for handling the authentication of the user.
  */
@@ -12,7 +14,24 @@ class AuthenticationService{
         localStorage.removeItem('authToken');
     }
     isAuthenticated(){
-        return !!this.getAuthToken();
+        /* this method will make sure there is an authenticated user */
+        /* first get the token from the local storage */
+        const token = this.getAuthToken();
+        /* if there is no token, then the user is not authenticated */
+        if(!token){
+            return false;
+        }
+        /* if there is a token, make sure this is a valid JWT token */
+        try{
+            const decoded = decode(token);
+            /* if the token is valid, then the user is authenticated */
+            if ( decoded.exp > (Date.now() / 1000) ){
+                return true;
+            }
+            else return false;
+        }catch(error){
+            return false;
+        }
     }
     logout(){
         this.removeAuthToken();
