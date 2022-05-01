84c0566f18e69a866eff469be46c3b108d5d5fb1
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-05-01 16:26:24 +0300

    develop RecommendedPostsBanner component

--- src/components/Posts/RecommendedPostsBanner/RecommendedPostsBanner.js
+++ src/components/Posts/RecommendedPostsBanner/RecommendedPostsBanner.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useState ,useEffect} from "react";
+/* Services */
+import { PostsService } from "../../../services";
+/* React Bootstrap Components */
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import {Link} from "react-router-dom";
+import Image from "react-bootstrap/Image";
+/* Styles */
+import "./RecommendedPostsBanner.css";
 
 const RecommendedPostsBanner = () => {
 
+    const [recommendedPosts, setRecommendedPosts] = useState([]);
+
+    useEffect(async () => {
+        const posts = await PostsService.getRecommendedPostsForBanner();
+        setRecommendedPosts(posts);
+    }, []);
+
     return (
-        <div>RecommendedPostsBanner</div>
+        <Container>
+            <Row>
+                {
+                    recommendedPosts.map((post, index) => (
+                        <Col key={index} md={4} className="mb-4">
+                            <Container className="p-0">
+                                <Card style={{ maxHeight:350, height: 300 }} className="align-content-center p-2">
+                                    <Image src={post.mainImage} fluid style={{ maxHeight:180, height: 250 }}></Image>
+                                    <Container className="align-content-center pt-3 p-0">
+                                        <a href={`/posts/${post._id}`}>{post.title}</a>
+                                    </Container>
+                                </Card>
+                            </Container>
+                        </Col>
+                    ))
+                }
+            </Row>
+        </Container>
     )
 }
 
