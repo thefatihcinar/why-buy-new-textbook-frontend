cf025fe2e0d7f918d4c96fccf17bf50e30bd6e97
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-05-03 15:59:14 +0300

    implementing and styling footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+
 
 const Footer = () => {
-
-  return (
-      <div>FOOTER</div>
-  );
+    return (
+      <footer>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect fixed="bottom" style={{height: "2rem"}}>
+          <Container>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text className="py-0 mb-0" >
+                Copyright &copy; Ankara Üniversitesi
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </footer>       
+    )
 }
 
 export default Footer;
+
