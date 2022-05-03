2b3f5265b6dafa4ccd556b873ff0672ae6433b68
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-05-03 22:12:02 +0300

    400 NotFound component implemented

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+
 
 const NotFound = () => {
 
     return (
-        <div>404 - NotFound</div>
+        <Container style={{ maxWidth: 600}}>
+            <Row>
+                <Image src="/assets/404.jpg" fluid/>
+                <h1>Aradığınız sayfa bulunamadı.</h1>
+            </Row>
+            <Row className="mb-5">
+
+            </Row>
+        </Container>
     )
 }
 
