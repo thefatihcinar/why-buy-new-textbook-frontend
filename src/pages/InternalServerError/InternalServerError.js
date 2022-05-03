b36ef3c2c6504dd395956dd608002825b1519102
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-05-03 22:12:13 +0300

    500 ServerError component implemented

--- src/pages/InternalServerError/InternalServerError.js
+++ src/pages/InternalServerError/InternalServerError.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 
 const InternalServerError = () => {
 
     return (
-        <div>500 - InternalServerError</div>
+        <Container style={{ maxWidth: 600}} className="mt-5 mb-5">
+            <Row className="mt-2">
+                <Image src="/assets/500.jpg" fluid/>
+                <h1>Sunucu Hatası.</h1>
+            </Row>
+            <Row className="mb-5">
+            </Row>
+        </Container>
     )
 }
 
