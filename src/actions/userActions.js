b1a5b75fb592a64f0c96cf6008d2ee016122a360
Fatih Çınar <rea.fatihcinar@gmail.com>
2022-05-04 03:06:41 +0300

    store token explicitly when user is logged in

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { USER_LOGIN_FAILURE, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, 
+import { USER_LOGIN_FAILURE, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT,
          USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAILURE } from '../constants/userConstants'
 import UsersService from '../services/users.service'
 
@@ -12,11 +12,12 @@ export const login = (email, password) => async(dispatch) => {
         dispatch( { type: USER_LOGIN_REQUEST } )
 
         let data  = await UsersService.loginUser(email, password);
-        
+
         dispatch( { type: USER_LOGIN_SUCCESS, payload: data } )
 
         // Remember the user, in local storage
         localStorage.setItem('userInfo', JSON.stringify(data));
+        localStorage.setItem('authToken', data.token);
 
     } catch (error) {
         // when the authentication fails
@@ -46,20 +47,20 @@ export const register = (name, email, password) => async(dispatch) => {
         dispatch( { type: USER_REGISTER_REQUEST } );
 
         let data  = await UsersService.registerUser(name, email, password);
-        
+
         dispatch( { type: USER_REGISTER_SUCCESS, payload: data } );
 
         /* Now, this is important
            Also dispatch, i.e. FIRE LOGIN SUCCESS ACTION WITH THIS USER AS WELL
            TO INDICATE THAT HE HAS AUTOMATICALLY LOGGED IN */
-        
+
         dispatch( { type: USER_LOGIN_SUCCESS, payload: data } )
 
         // again add the logged in user to the local storage
         localStorage.setItem('userInfo', JSON.stringify(data));
 
     } catch (error) {
-        
+
         dispatch( { type: USER_REGISTER_FAILURE,
                     payload: error.response && error.response.data.message
                              ? error.response.data.message
