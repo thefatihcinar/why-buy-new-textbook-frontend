4850fbd1fda1f2f41340874ce48378f284519acf
BenguTercan <62440074+BenguTercan@users.noreply.github.com>
2022-05-05 09:41:20 +0300

    alignment in user reducers

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -3,30 +3,30 @@ import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT
 
 export const userLoginReducer = (state = {} , action) => {
 /* this reducer controls the user login state throughout the application */
-switch (action.type) {
-case USER_LOGIN_REQUEST:
-  return { loading: true };
-case USER_LOGIN_SUCCESS:
-  return { loading: false, userInfo: action.payload};
-case USER_LOGIN_FAILURE:
-  return { loading: false, error: action.payload};
-case USER_LOGOUT:
-  return { };
-default:
-  return state;
-}
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true };
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload};
+    case USER_LOGIN_FAILURE:
+      return { loading: false, error: action.payload};
+    case USER_LOGOUT:
+      return { };
+    default:
+      return state;
+  }
 }
 
 export const userRegisterReducer = (state = {} , action) => {
 /* this reducer controls the user register state */
-switch (action.type) {
-   case USER_REGISTER_REQUEST:
-       return { loading: true };
-   case USER_REGISTER_SUCCESS:
-       return { loading: false, userInfo: action.payload };
-   case USER_REGISTER_FAILURE:
-       return { loading: false, error: action.payload};
-   default:
-       return state;
-}
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+        return { loading: true };
+    case USER_REGISTER_SUCCESS:
+        return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAILURE:
+        return { loading: false, error: action.payload};
+    default:
+        return state;
+  }
 }
